6e6a6f8c50a210f76016d470fa0e9d10e4300e71
Ben-Amin York <ybenamin@amazon.com>
2025-05-06 19:05:50 -0500

    app domain

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -296,7 +296,7 @@ export const qWebExperience = new q.CfnWebExperience(customResource, "QWebExperi
   applicationId: qapp.attrApplicationId,
   origins: [
     /* REPLACE WITH YOUR AMPLIFY DOMAIN ENDPOINT */
-    "https://main.your-app-url.amplifyapp.com",
+    "https://main.d34bxanjhhvf5j.amplifyapp.com/",
   ],
   samplePromptsControlMode: "ENABLED",
   subtitle: "AnyCompany Generative AI Assistant",
